19a87868c97c75e4e92b1d021b039bd1c4eec868
Alexandre Liscia <alexandre.liscia@orinoko.fr>
2020-11-03 15:10:56 +0100

    Add main layout and style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,283 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header class="header">
+    <div class="topbar">
+      <div class="logo">
+        <a href="/">Céline coach bien-être</a>
+      </div>
+
+      <nav class="menu">
+        <a href="#" class="menu-item">A propos</a>
+        <a href="#" class="menu-item">Contact</a>
+        <a href="#" class="menu-item menu-button">Réserver</a>
+      </nav>
+    </div>
+    <div class="baseline reveal">
+      <span class="baseline-welcome reveal-1">Bienvenue</span>
+      <strong class="baseline-name reveal-2">Céline Coach bien-être</strong>
+      <span class="baseline-slogan reveal-3"
+      >Soins - Esthétique - Diagnostique de peau en ligne</span
+      >
+    </div>
+  </header>
+
+  <Welcome/>
+
+  <footer class="footer reveal" id="footer">
+    <div class="container">
+      <div class="footer-columns">
+        <div class="footer-column reveal-1">
+          <div class="footer-title">Céline Coach bien-être</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+            dolorum, sint corporis nostrum, possimus unde eos vitae eius quasi
+            saepe.
+          </p>
+        </div>
+        <div class="footer-column reveal-2">
+          <div class="footer-title">Contact info</div>
+          <p>
+            1234 Altschul, New York, NY 10027-0000<br/>
+            +1 987 654 3210<br/>
+            <a href="mailto:contact@celine-coach.fr">contact@thefiasco.com</a>
+          </p>
+        </div>
+        <div class="footer-column reveal-3">
+          <div class="footer-title">Heures d'ouverture</div>
+          <ul>
+            <li>Lundi au Vendredi | 10:00 - 19:00</li>
+            <li>Lundi au Vendredi | 10:00 - 19:00</li>
+            <li>Lundi au Vendredi | 10:00 - 19:00</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import Welcome from "./components/Welcome.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Welcome,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.header {
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.92),
+      rgba(54, 54, 54, 0.5) 39.24%,
+      rgba(28, 28, 28, 0.4)
+  ),
+  url(./assets/banner.jpg) center center / cover no-repeat;
+  color: #fff;
+}
+
+.topbar {
+  padding: 18px 20px;
+  font-size: 0.875rem;
+  text-transform: uppercase;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.logo {
+  margin-bottom: 15px;
+}
+
+.menu-item {
+  display: inline-block;
+  margin: 0 15px;
+}
+
+.menu-item:hover {
+  color: #e387a6;
+}
+
+.menu-button {
+  border: solid 1px #e387a6;
+  padding: 7px 10px;
+  transition-property: color, background-color;
+  margin-top: 5px;
+}
+
+.menu-button:hover {
+  background: #e387a6;
+  color: #fff;
+}
+
+.baseline {
+  padding: 50px 0 70px 0;
+  text-align: center;
+  line-height: 1;
+}
+
+.baseline-welcome {
+  font-family: "Pinyon Script", serif;
+  color: #e387a6;
+  display: block;
+  font-size: 2.2rem;
+}
+
+.baseline-name {
+  display: block;
+  font-size: 2.5rem;
+  font-weight: 700;
+  letter-spacing: 5px;
+  padding-bottom: 10px;
+  text-transform: uppercase;
+}
+
+.baseline-slogan {
+  font-size: 1rem;
+  font-weight: 300;
+  display: block;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+
+.footer {
+  background: #383838 bottom right no-repeat;
+  font-weight: 300;
+  color: #fff;
+  padding: 40px 0;
+}
+
+.footer-title {
+  color: #e387a6;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 1.25em;
+}
+
+.footer-column {
+  margin-bottom: 2rem;
+}
+
+@media only screen and (min-width: 600px) {
+  .logo {
+    margin-bottom: 0;
+  }
+
+  .topbar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1150px;
+    margin: 0 auto;
+  }
+
+  .baseline {
+    padding: 125px 0 230px;
+  }
+
+  .baseline-slogan {
+    font-size: 1.5rem;
+  }
+
+  .baseline-name {
+    font-size: 4.375rem;
+    letter-spacing: 12px;
+    padding-top: 8px;
+    padding-bottom: 18px;
+  }
+
+  .baseline-welcome {
+    font-size: 3.75rem;
+  }
+
+  .menu-button {
+    margin-top: 0;
+    margin-right: 0;
+  }
+}
+
+@media only screen and (min-width: 800px) {
+  .footer {
+    background-image: url(./assets/footer-splash.jpg);
+  }
+
+  .footer-columns {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .footer-column {
+    width: 290px;
+    margin-right: 50px;
+  }
+
+  .footer-column:first-child {
+    width: 450px;
+  }
+
+  .footer-column:last-child {
+    margin-right: 0;
+    flex: none;
+  }
+
+  .footer a {
+    text-decoration: underline;
+  }
+
+  .footer a:hover {
+    color: #e387a6;
+  }
+
+  .footer ul {
+    padding-left: 0;
+    list-style: none;
+  }
+
+  .footer ul li::before {
+    content: "- ";
+  }
+
+  .footer-credits {
+    font-size: 0.93em;
+  }
+
+  .footer-credits span {
+    font-weight: bold;
+  }
+}
+
+.reveal-loaded .reveal [class*="reveal-"] {
+  opacity: 0 !important;
+  transform: translateY(30px) !important;
+  transition: 0s !important;
+}
+
+.reveal-loaded [class*="reveal-"] {
+  transition: 1s cubic-bezier(.5, 0, 0, 1);
+}
+
+.reveal-loaded .reveal-2 {
+  transition-delay: .1s;
+}
+
+.reveal-loaded .reveal-3 {
+  transition-delay: .2s;
+}
+
+.reveal-loaded .reveal-4 {
+  transition-delay: .3s;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .reveal [class*="reveal-"] {
+    opacity: 1 !important;
+    transform: translateY(0px) !important;
+  }
+
+  [class*="reveal-"] {
+    transition-duration: 0s !important;
+    transition-delay: 0s !important;
+  }
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,411 @@
+<template>
+  <section class="home-section">
+    <div class="container-small">
+      <div class="block block-left reveal">
+        <div class="block-img reveal-1">
+          <img src="../assets/image.jpg" alt=""/>
+        </div>
+        <div class="block-body">
+          <h2 class="title reveal-2">
+            <span>Découvrez</span> notre gamme beauté
+          </h2>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget. Duis
+            dictum faucibus pulvinar. Ut in nunc sit amet tellus tempus
+            convallis ac quis massa. Nullam lacinia semper massa, quis ultricies
+            arcu libero.
+          </p>
+          <div class="more reveal-4">
+            <a href="#">Voir</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="block block-right reveal">
+        <div class="block-img reveal-1">
+          <img src="../assets/image.jpg" alt=""/>
+        </div>
+        <div class="block-body">
+          <h2 class="title reveal-2">
+            <span>Découvrez</span> notre gamme beauté
+          </h2>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget. Duis
+            dictum faucibus pulvinar. Ut in nunc sit amet tellus tempus
+            convallis ac quis massa. Nullam lacinia semper massa, quis ultricies
+            arcu libero.
+          </p>
+          <div class="more reveal-4">
+            <a href="#">Voir</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="block block-centered block-last reveal">
+        <div class="block-body">
+          <h2 class="title reveal-1">
+            <span>Massages</span> certifiés détente
+          </h2>
+          <p class="reveal-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget. Duis
+            dictum faucibus pulvinar. Ut in nunc sit amet tellus tempus
+            convallis ac quis massa. Nullam lacinia semper massa, quis ultricies
+            arcu libero.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="menu-summary" id="menu">
+        <div class="menu-summary-item reveal">
+          <img
+              src="../assets/image.jpg"
+              alt=""
+              class="menu-summary-img reveal-1"
+          />
+          <h3 class="menu-summary-title reveal-2">Massage génial</h3>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget.
+          </p>
+          <div class="menu-summary-price reveal-4">90€</div>
+        </div>
+        <div class="menu-summary-item reveal">
+          <img
+              src="../assets/image.jpg"
+              alt=""
+              class="menu-summary-img reveal-1"
+          />
+          <h3 class="menu-summary-title reveal-2">Massage génial</h3>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget.
+          </p>
+          <div class="menu-summary-price reveal-4">90€</div>
+        </div>
+        <div class="menu-summary-item reveal">
+          <img
+              src="../assets/image.jpg"
+              alt=""
+              class="menu-summary-img reveal-1"
+          />
+          <h3 class="menu-summary-title reveal-2">Massage génial</h3>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget.
+          </p>
+          <div class="menu-summary-price reveal-4">90€</div>
+        </div>
+        <div class="menu-summary-item reveal">
+          <img
+              src="../assets/image.jpg"
+              alt=""
+              class="menu-summary-img reveal-1"
+          />
+          <h3 class="menu-summary-title reveal-2">Massage génial</h3>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget.
+          </p>
+          <div class="menu-summary-price reveal-4">90€</div>
+        </div>
+      </div>
+
+      <div class="block block-celine">
+        <div class="block-img">
+          <img src="../assets/celine.png" alt=""/>
+        </div>
+        <div class="block-body reveal">
+          <h2 class="title reveal-1">
+            <span>Hôtesse</span> RENCONTREZ-MOI, CELINE TERTRAIS
+          </h2>
+          <p class="reveal-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget. Duis
+            dictum faucibus pulvinar. Ut in nunc sit amet tellus tempus
+            convallis ac quis massa. Nullam lacinia semper massa, quis ultricies
+            arcu libero.
+          </p>
+          <p class="reveal-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            consectetur magna mauris, sed maximus turpis tristique eget. Duis
+            dictum faucibus pulvinar.
+          </p>
+          <div class="more reveal-4">
+            <a href="#">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class Welcome extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home-section {
+  padding-top: 9px;
+}
+
+.container-small {
+  margin: 0 auto;
+  padding: 0 20px;
+  max-width: 960px;
+}
+
+.container {
+  margin: 0 auto;
+  padding: 0 20px;
+  max-width: 1150px;
+}
+
+.block {
+  margin-bottom: 50px;
+}
+
+.block-centered {
+  text-align: center;
+}
+
+.block-centered .block-body {
+  max-width: 640px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.block-img {
+  max-width: 350px;
+  margin: 0 auto;
+  border: solid 1px #e387a6;
+}
+
+.block-img img {
+  display: block;
+}
+
+.block-img img {
+  position: relative;
+  z-index: 1;
+}
+
+.title {
+  font-size: 1.75rem;
+  line-height: 1.1;
+  letter-spacing: 2px;
+  color: #000;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 1rem 0 0 0;
+}
+
+.title span {
+  font-family: "Pinyon Script", serif;
+  color: #e387a6;
+  font-size: 2.5rem;
+  display: block;
+  text-transform: none;
+}
+
+.title::before {
+  content: "";
+  display: inline-block;
+  width: 24px;
+  height: 2px;
+  background: #e387a6;
+  margin-bottom: 0.5rem;
+}
+
+.more {
+  color: #e387a6;
+}
+
+.menu-summary-item {
+  position: relative;
+  padding-left: 120px;
+  margin-bottom: 30px;
+}
+
+.menu-summary-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100px;
+}
+
+.menu-summary-title {
+  font-size: 1.125rem;
+  color: #383838;
+  font-weight: bold;
+  padding-bottom: 0.75rem;
+  margin-bottom: 0;
+}
+
+.menu-summary p {
+  margin: 0;
+  padding-bottom: 1rem;
+  font-size: 0.875rem;
+}
+
+.menu-summary-price {
+  font-family: "Pinyon Script", serif;
+  color: #e387a6;
+  font-size: 2.5rem;
+  line-height: 1;
+}
+
+.reservation .block-centered .block-body {
+  max-width: 825px;
+}
+
+@media only screen and (min-width: 800px) {
+  .block {
+    margin-bottom: 100px;
+  }
+
+  .block-last {
+    margin-bottom: 60px;
+  }
+
+  .block-left,
+  .block-right {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .block-img {
+    border: none;
+    position: relative;
+  }
+
+  .block-left .block-body,
+  .block-right .block-body {
+    width: 445px;
+    flex: none;
+  }
+
+  .block-img::before {
+    content: "";
+    display: block;
+    border: solid 1px #e387a6;
+    position: absolute;
+    top: -30px;
+    left: -30px;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+  }
+
+  .block-right .block-img::before {
+    left: auto;
+    right: -30px;
+  }
+
+  .block-left .block-img {
+    margin-left: 30px;
+  }
+
+  .block-right .block-img {
+    margin-right: 30px;
+  }
+
+  .block-left .block-body {
+    margin-left: 20px;
+  }
+
+  .block-right .block-body {
+    margin-right: 20px;
+    order: -1;
+  }
+
+  .menu-summary {
+    position: relative;
+    padding: 85px 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  .menu-summary-item {
+    padding-left: 192px;
+    margin-bottom: 110px;
+    width: calc(50% - 40px);
+  }
+
+  .menu-summary .menu-summary-item:nth-child(even) {
+    text-align: right;
+    padding-left: 0;
+    padding-right: 192px;
+  }
+
+  .menu-summary .menu-summary-item:nth-child(even) .menu-summary-img {
+    left: auto;
+    right: 0;
+  }
+
+  .menu-summary .menu-summary-item:nth-child(even) .menu-summary-price {
+    margin-left: auto;
+  }
+
+  .menu-summary-img {
+    width: 160px;
+  }
+
+  .menu-summary-title {
+    margin-top: 0;
+    line-height: 1.2;
+    background: #ededed;
+  }
+
+  .menu-summary p {
+    background: #ededed;
+  }
+
+  .menu-summary-price {
+    padding-top: 4px;
+    width: 85px;
+    background: #ededed;
+  }
+
+  .menu-summary::before {
+    content: "";
+    position: absolute;
+    display: block;
+    top: 50%;
+    left: 50%;
+    width: 420px;
+    height: 420px;
+    border: solid 1px #e387a6;
+    transform: translate(-50%, -260px) rotate(45deg);
+  }
+
+  .block-celine {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0px;
+  }
+
+  .block-celine .block-img::before {
+    display: none;
+  }
+
+  .block-celine .block-img {
+    flex: none;
+    width: 404px;
+    order: 2;
+  }
+}
+</style>
